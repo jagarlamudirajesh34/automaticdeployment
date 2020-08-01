11c976fd5a33efc0908fc5d0f13156f339ea2698
Administrator <admin@example.com>
2020-08-01 21:26:40 +0530

    bvvhhvhvhvhvhvhvvhvhvh

--- src/main/java/com/test/rest/controller/UserController.java
+++ src/main/java/com/test/rest/controller/UserController.java
@@ -43,7 +43,7 @@ public class UserController {
 			e.printStackTrace();
 			serviceStatus.setStatus("failure");
 			serviceStatus.setMessage("Exception occured");
-			serviceStatus.setResult(e.getStackTrace());
+			serviceStatus.setResult(e.getLocalizedMessage());
 		}
 		return serviceStatus;
 	}
